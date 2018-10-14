01f319a66b817ebfdd4585a9ea34177a2699275d
Julián Mitchell <rhuxell@gmail.com>
2018-10-14 18:26:40 -0300

    agrego nunjucks

--- servidor.js
+++ servidor.js
@@ -1,11 +1,16 @@
 var express = require("express");
+var nunjucks = require("nunjucks");
 
 var app = express();
 
+nunjucks.configure(__dirname + "/vistas",{
+    express:app
+});
+
 app.listen(8084);
 
-app.get("/inicio", function(req, res){
-    res.send("Holis!");
+app.get("/articulo", function(req, res){
+    res.render("articulo.html");
 });
 
 app.get("/informes", function(req, res){
